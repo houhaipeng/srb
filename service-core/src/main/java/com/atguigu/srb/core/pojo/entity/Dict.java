7d611cee70c79aa0b1f36a90fcd3accdf01fc44a
houhaipeng <1439801553@qq.com>
2021-05-30 20:41:56 +0800

    srb

--- service-core/src/main/java/com/atguigu/srb/core/pojo/entity/Dict.java
+++ service-core/src/main/java/com/atguigu/srb/core/pojo/entity/Dict.java
@@ -1,15 +1,16 @@
 package com.atguigu.srb.core.pojo.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -43,7 +44,7 @@ public class Dict implements Serializable {
     private String dictCode;
 
     @ApiModelProperty(value = "创建时间")
-    private LocalDateTime createTime;
+    private LocalDateTime createTime;//代表了Date
 
     @ApiModelProperty(value = "更新时间")
     private LocalDateTime updateTime;
